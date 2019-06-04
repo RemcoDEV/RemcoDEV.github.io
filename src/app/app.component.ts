850504902f1ced65e6e4b78752fc1f19a55ccdf6
Unknown <remcovogelzang@ziggo.nl>
2019-06-04 15:27:34 +0200

    Angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  constructor() {
+
+  }
+
+  getYear() {
+      return new Date().getFullYear();
+  }
+}
