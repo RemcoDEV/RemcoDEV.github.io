85199152bee574ec618e71ac516f777cc611ca46
Unknown <remcovogelzang@ziggo.nl>
2019-06-15 16:07:21 +0200

    Iframe test 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
   menuOpen: boolean = false;
 
   constructor() {
-    this.mobileDevice = top.innerWidth <= 845;
+    this.mobileDevice = self.innerWidth <= 845;
   }
 
   getYear() {
@@ -23,7 +23,7 @@ export class AppComponent {
   }
 
   onResize(event) {
-    this.mobileDevice = top.innerWidth <= 845;
+    this.mobileDevice = self.innerWidth <= 845;
   }
 
   openMenuOverlay(open: boolean) {
